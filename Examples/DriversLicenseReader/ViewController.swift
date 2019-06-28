69628aaa0459961625353d9e8226da24e49e6603
treastrain / Tanaka Ryoga <tr@tret.jp>
2019-06-28 16:14:33 +0900

    運転免許証リーダーサンプルの作成

---
+++ Examples/DriversLicenseReader/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  DriversLicenseReader
+//
+//  Created by treastrain on 2019/06/12.
+//  Copyright © 2019 treastrain / Tanaka Ryoga. All rights reserved.
+//
+
+import UIKit
+import TRETJapaneseNFCReader
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func beginScanning() {
+    }
+    
+    
+}
+
