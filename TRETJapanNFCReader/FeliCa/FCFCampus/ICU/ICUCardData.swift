132b372c5a7cf5b06d08af0787735ba936790ceb
Qs-F <qsf@de-liker.com>
2019-09-27 03:15:56 +0900

    ICU: Add ICUCardData.swift

---
+++ TRETJapanNFCReader/FeliCa/FCFCampus/ICU/ICUCardData.swift
@@ -0,0 +1,12 @@
+//
+//  ICUCardData.swift
+//  TRETJapanNFCReader
+//
+//  Created by Qs-F on 2019/09/26.
+//  Copyright © 2019 treastrain / Tanaka Ryoga. All rights reserved.
+//
+
+import Foundation
+
+public struct ICUCardData: FeliCaCardData {
+}
