c60a596e0a7a23d2f4ec5c93a7b459a10a5e80e2
treastrain / Tanaka Ryoga <tr@tret.jp>
2019-11-09 04:09:53 +0900

    エヌタスTカードの対応作業を開始

---
+++ TRETJapanNFCReader/FeliCa/NTasu/NTasuReader.swift
@@ -0,0 +1,35 @@
+//
+//  NTasuReader.swift
+//  TRETJapanNFCReader
+//
+//  Created by treastrain on 2019/10/20.
+//  Copyright © 2019 treastrain / Tanaka Ryoga. All rights reserved.
+//
+
+import CoreNFC
+
+@available(iOS 13.0, *)
+public typealias NTasuCardTag = NFCFeliCaTag
+
+@available(iOS 13.0, *)
+public class NTasuReader: FeliCaReader {
+    
+    private var nTasuCardItemTypes: [NTasuCardItemType] = []
+    
+    private init() {
+        fatalError()
+    }
+    
+    public init(feliCaReader: FeliCaReader) {
+        super.init(delegate: feliCaReader.delegate!)
+    }
+    
+    public override init(delegate: FeliCaReaderSessionDelegate) {
+        super.init(delegate: delegate)
+    }
+    
+    public override init(viewController: FeliCaReaderViewController) {
+        super.init(viewController: viewController)
+    }
+    
+}
