1c2883f5db134d3854b5ec26a2a9be5456242453
treastrain / Tanaka Ryoga <tr@tret.jp>
2019-11-29 13:34:14 +0900

    SPM 対応に向けたパスの変更

---
+++ Package.swift
@@ -0,0 +1,25 @@
+// swift-tools-version:5.1
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "TRETJapanNFCReader",
+    platforms: [
+        .iOS("9.3"),
+        .watchOS(.v2),
+        .tvOS("9.2"),
+        .macOS(.v10_10),
+    ],
+    products: [
+        .library(
+            name: "TRETJapanNFCReader",
+            targets: ["TRETJapanNFCReader"]),
+    ],
+    targets: [
+        .target(
+            name: "TRETJapanNFCReader",
+            dependencies: []),
+    ],
+    swiftLanguageVersions: [.v5]
+)
