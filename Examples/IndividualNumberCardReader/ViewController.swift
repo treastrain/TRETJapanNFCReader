b8afee7382671a3f6a596f384461bdadaa15aabd
treastrain / Tanaka Ryoga <tr@tret.jp>
2020-05-10 17:43:59 +0900

    Examples/IndividualNumberCardReader の追加

---
+++ Examples/IndividualNumberCardReader/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IndividualNumberCardReader
+//
+//  Created by treastrain on 2020/05/10.
+//  Copyright © 2020 treastrain / Tanaka Ryoga. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
