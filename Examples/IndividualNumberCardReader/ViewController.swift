5f162c7965a16c36b4d01ac3e9efff6258015141
treastrain / Tanaka Ryoga <tr@tret.jp>
2020-05-11 17:43:38 +0900

    券面事項入力補助用アプリのPIN番号入力 TextField を追加

--- Examples/IndividualNumberCardReader/ViewController.swift
+++ Examples/IndividualNumberCardReader/ViewController.swift
@@ -13,18 +13,25 @@ class ViewController: UIViewController, IndividualNumberReaderSessionDelegate {
     
     var reader: IndividualNumberReader!
     
+    @IBOutlet weak var cardInfoInputSupportPINTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.execute()
     }
     
     @IBAction func execute() {
+        self.cardInfoInputSupportPINTextField.resignFirstResponder()
+        
+        let items: [IndividualNumberCardItem] = [.tokenInfo, .individualNumber]
+        /// 券面事項入力補助用アプリの PIN
+        let cardInfoInputSupportAppPIN = self.cardInfoInputSupportPINTextField.text ?? ""
+        
         self.reader = IndividualNumberReader(delegate: self)
-        self.reader.get()
+        self.reader.get(items: items, cardInfoInputSupportAppPIN: cardInfoInputSupportAppPIN)
     }
     
-    func individualNumberReaderSession(didRead individualNumberCard: IndividualNumberCard) {
-        print(individualNumberCard)
+    func individualNumberReaderSession(didRead individualNumberCardData: IndividualNumberCardData) {
+        print(individualNumberCardData)
     }
     
     func japanNFCReaderSession(didInvalidateWithError error: Error) {
