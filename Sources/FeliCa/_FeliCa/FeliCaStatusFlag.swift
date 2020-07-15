cff3fd61d9776164d0e51da4120c6cd7c0c383ee
treastrain / Tanaka Ryoga <tr@tret.jp>
2020-07-16 03:01:31 +0900

    Create FeliCaStatusFlag.swift

---
+++ Sources/FeliCa/_FeliCa/FeliCaStatusFlag.swift
@@ -0,0 +1,32 @@
+//
+//  FeliCaStatusFlag.swift
+//  TRETJapanNFCReader
+//
+//  Created by treastrain on 2020/07/16.
+//
+
+#if os(iOS)
+import CoreNFC
+
+@available(iOS 13.0, *)
+public struct FeliCaStatusFlag {
+    public var statusFlag1: Int
+    public var statusFlag2: Int
+    
+    init(_ statusFlag1: Int, _ statusFlag2: Int) {
+        self.statusFlag1 = statusFlag1
+        self.statusFlag2 = statusFlag2
+    }
+    
+    @available(iOS 14.0, *)
+    init(_ statusFlag: CoreNFC.NFCFeliCaStatusFlag) {
+        self.statusFlag1 = statusFlag.statusFlag1
+        self.statusFlag2 = statusFlag.statusFlag2
+    }
+    
+    public var isSucceeded: Bool {
+        return statusFlag1 == 0x00 && statusFlag2 == 0x00
+    }
+}
+
+#endif
