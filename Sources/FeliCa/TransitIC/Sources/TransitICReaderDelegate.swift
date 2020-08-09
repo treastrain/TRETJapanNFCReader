787a10ae5a197f1c27d27f5e4f32e18ffbd85d80
treastrain / Tanaka Ryoga <tr@tret.jp>
2020-08-10 08:07:53 +0900

    Update TransitICReaderDelegate.swift

--- Sources/FeliCa/TransitIC/Sources/TransitICReaderDelegate.swift
+++ Sources/FeliCa/TransitIC/Sources/TransitICReaderDelegate.swift
@@ -6,6 +6,7 @@
 //  Copyright © 2020 treastrain / Tanaka Ryoga. All rights reserved.
 //
 
+#if os(iOS)
 import Foundation
 #if canImport(TRETJapanNFCReader_Core)
 import TRETJapanNFCReader_Core
@@ -19,3 +20,5 @@ public protocol TransitICReaderDelegate: FeliCaReaderDelegate {
     func readerSessionDidBecomeActive()
     func readerSessionReadDidInvalidate(with result: Result<TransitICCardDataResponse, Error>)
 }
+
+#endif
