a7fd7c8fae6eca3bb9d392cce7051fed80754c44
treastrain / Tanaka Ryoga <tr@tret.jp>
2020-08-27 17:06:26 +0900

    JapanNFCReaderError の LocalizedError 対応を開始

--- Sources/Core/JapanNFCReaderError.swift
+++ Sources/Core/JapanNFCReaderError.swift
@@ -22,3 +22,36 @@ public enum JapanNFCReaderError: Error {
     /// An error that occurred in `NFCTagReaderSession.connect(to:completionHandler:)` method.
     case tagReaderSessionConnectError(_ nfcReaderError: Error)
 }
+
+@available(iOS 13.0, *)
+extension JapanNFCReaderError: LocalizedError {
+    public var errorDescription: String? {
+        switch self {
+        case .readingUnavailable:
+            return "NFC Tag Reading is unavailable."
+        case .couldNotCreateTagReaderSession:
+            return nil
+        case .invalidDetectedTagType:
+            return nil
+        case .tagReaderSessionDidInvalidateWithError(_):
+            return nil
+        case .tagReaderSessionConnectError(_):
+            return nil
+        }
+    }
+    
+    public var failureReason: String? {
+        switch self {
+        case .readingUnavailable:
+            return "This device does not support NFC tag reading."
+        case .couldNotCreateTagReaderSession:
+            return nil
+        case .invalidDetectedTagType:
+            return nil
+        case .tagReaderSessionDidInvalidateWithError(_):
+            return nil
+        case .tagReaderSessionConnectError(_):
+            return nil
+        }
+    }
+}
