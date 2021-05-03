64fabc5761ca20fa9ab4dd0d8e89b905b66c102b
treastrain / Tanaka Ryoga <tr@tret.jp>
2021-05-03 22:04:55 +0900

    Delete heritage

--- Sources/MIFARE/_MIFARE/MIFARE.swift
+++
@@ -1,9 +0,0 @@
-//
-//  MIFARE.swift
-//  TRETJapanNFCReader
-//
-//  Created by treastrain on 2020/05/10.
-//  Copyright © 2020 treastrain / Tanaka Ryoga. All rights reserved.
-//
-
-import Foundation
