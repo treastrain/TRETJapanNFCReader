32a41ecb4e7c36d4f8ac0f53f3fc91d6d8c79f2e
treastrain / Tanaka Ryoga <tr@tret.jp>
2021-05-08 04:17:51 +0900

    Update NFCTagReaderSession+PollingOption.swift

--- Sources/NFCKit/Structures/NFCTagReaderSession+PollingOption.swift
+++ Sources/NFCKit/Structures/NFCTagReaderSession+PollingOption.swift
@@ -11,6 +11,9 @@ import CoreNFC
 #endif
 
 public extension NFCTagReaderSession {
+    /// Options that determine the type of tags that a reader session should detect during a polling sequence.
+    ///
+    /// You can combine options to have the reader session scan and detect different tag types at the same time.
     struct PollingOption : OptionSet, Codable {
         public init(rawValue: Int) {
             self.rawValue = rawValue
@@ -23,8 +26,15 @@ public extension NFCTagReaderSession {
         }
         #endif
         
+        /// The option for detecting ISO 7816-compatible and MIFARE tags.
+        ///
+        /// Supports NFC type A and B modulation.
         public static var iso14443 = Self(rawValue: 1 << 0)
+        
+        /// The option for detecting ISO 15693 tags.
         public static var iso15693 = Self(rawValue: 1 << 1)
+        
+        /// The option for detecting FeliCa tags.
         public static var iso18092 = Self(rawValue: 1 << 2)
         
         public var rawValue: Int
