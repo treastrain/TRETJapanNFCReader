df0108d30e11001f8dae1dd59f353f453b7daaf6
treastrain / Tanaka Ryoga <tr@tret.jp>
2021-05-16 04:19:05 +0900

    Update FeliCaTag+Polling.swift

--- Sources/NFCKit/Structures/FeliCaTag+Polling.swift
+++ Sources/NFCKit/Structures/FeliCaTag+Polling.swift
@@ -21,54 +21,55 @@ extension FeliCaTag {
     ///   - resultHandler: Returns `FeliCaPollingResponse` or a `NFCErrorDomain` error when the operation is completed. Valid `requestData` is return when `requestCode` is a non-zero parameter and feature is supported by the tag. The `currentIDm` property will be updated on each execution, except when an invalid `systemCode` is provided and the existing selected system will stay selected.
     ///
     /// System code must be one of the provided values in the "com.apple.developer.nfc.readersession.felica.systemcodes" in the Info.plist; `NFCReaderErrorSecurityViolation` will be returned when an invalid system code is used. Polling with wildcard value in the upper or lower byte is not supported.
+    @available(iOS 13.0, *)
+    @available(watchOS, unavailable)
+    @available(tvOS, unavailable)
+    @available(macOS, unavailable)
+    @available(macCatalyst, unavailable)
     public func polling(systemCode: Data, requestCode: FeliCaPollingRequestCode, timeSlot: FeliCaPollingTimeSlot, resultHandler: @escaping (Result<FeliCaPollingResponse, Error>) -> Void) {
         
         #if os(iOS) && !targetEnvironment(macCatalyst)
-        if #available(iOS 13.0, *) {
-            let coreRequestCode: PollingRequestCode
-            switch requestCode {
-            case .noRequest:
-                coreRequestCode = .noRequest
-            case .systemCode:
-                coreRequestCode = .systemCode
-            case .communicationPerformance:
-                coreRequestCode = .communicationPerformance
-            }
-            
-            let coreTimeSlot: PollingTimeSlot
-            switch timeSlot {
-            case .max1:
-                coreTimeSlot = .max1
-            case .max2:
-                coreTimeSlot = .max2
-            case .max4:
-                coreTimeSlot = .max4
-            case .max8:
-                coreTimeSlot = .max8
-            case .max16:
-                coreTimeSlot = .max16
-            }
-            
-            if #available(iOS 14.0, *) {
-                self.core.polling(systemCode: systemCode, requestCode: coreRequestCode, timeSlot: coreTimeSlot) { result in
-                    switch result {
-                    case .success(let feliCaPollingResponse):
-                        resultHandler(.success(FeliCaPollingResponse(from: feliCaPollingResponse)))
-                    case .failure(let error):
-                        resultHandler(.failure(error))
-                    }
-                }
-            } else {
-                self.core.polling(systemCode: systemCode, requestCode: coreRequestCode, timeSlot: coreTimeSlot) { manufactureParameter, requestData, error in
-                    if let error = error {
-                        resultHandler(.failure(error))
-                    } else {
-                        resultHandler(.success(FeliCaPollingResponse(manufactureParameter: manufactureParameter, requestData: requestData)))
-                    }
+        let coreRequestCode: PollingRequestCode
+        switch requestCode {
+        case .noRequest:
+            coreRequestCode = .noRequest
+        case .systemCode:
+            coreRequestCode = .systemCode
+        case .communicationPerformance:
+            coreRequestCode = .communicationPerformance
+        }
+        
+        let coreTimeSlot: PollingTimeSlot
+        switch timeSlot {
+        case .max1:
+            coreTimeSlot = .max1
+        case .max2:
+            coreTimeSlot = .max2
+        case .max4:
+            coreTimeSlot = .max4
+        case .max8:
+            coreTimeSlot = .max8
+        case .max16:
+            coreTimeSlot = .max16
+        }
+        
+        if #available(iOS 14.0, *) {
+            self.core.polling(systemCode: systemCode, requestCode: coreRequestCode, timeSlot: coreTimeSlot) { result in
+                switch result {
+                case .success(let feliCaPollingResponse):
+                    resultHandler(.success(FeliCaPollingResponse(from: feliCaPollingResponse)))
+                case .failure(let error):
+                    resultHandler(.failure(error))
                 }
             }
         } else {
-            fatalError("\(#function): Not implemented")
+            self.core.polling(systemCode: systemCode, requestCode: coreRequestCode, timeSlot: coreTimeSlot) { manufactureParameter, requestData, error in
+                if let error = error {
+                    resultHandler(.failure(error))
+                } else {
+                    resultHandler(.success(FeliCaPollingResponse(manufactureParameter: manufactureParameter, requestData: requestData)))
+                }
+            }
         }
         #else
         fatalError("\(#function): Not implemented")
