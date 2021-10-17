8727a024b9bcc169ba40153fcc99d05ed49956c4
treastrain / Tanaka Ryoga <tr@tret.jp>
2021-10-18 02:38:34 +0900

    Create NFCKitReaderError.swift

---
+++ Sources/NFCKitReader/NFCKitReaderCore/NFCKitReaderError.swift
@@ -0,0 +1,15 @@
+//
+//  NFCKitReaderError.swift
+//  TRETNFCKit
+//
+//  Created by treastrain on 2021/10/17.
+//
+
+import NFCKitCore
+
+public enum NFCKitReaderError: Error {
+    /// [`NFCTagReaderSession.readingAvailable`](https://developer.apple.com/documentation/corenfc/nfcreadersession/3043845-readingavailable) is `false`.
+    case readingUnavailable
+    /// Could not initialize the reader session due to unavailability of system memory resources.
+    case systemMemoryResourcesAreUnavailable
+}
