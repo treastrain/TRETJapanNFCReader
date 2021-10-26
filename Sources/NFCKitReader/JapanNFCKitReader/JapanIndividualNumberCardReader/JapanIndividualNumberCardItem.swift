98ccb761b22e1a24799bf4c6ce8003cb578a1090
treastrain / Tanaka Ryoga <tr@tret.jp>
2021-10-27 00:31:38 +0900

    Update JapanIndividualNumberCardItem.swift

--- Sources/NFCKitReader/JapanNFCKitReader/JapanIndividualNumberCardReader/JapanIndividualNumberCardItem.swift
+++ Sources/NFCKitReader/JapanNFCKitReader/JapanIndividualNumberCardReader/JapanIndividualNumberCardItem.swift
@@ -8,26 +8,26 @@
 import NFCKitReaderCore
 
 public enum JapanIndividualNumberCardItem: CaseIterable {
-    /// 署名用電子証明書
-    case digitalSignature
-    /// 利用者証明用電子証明書
-    case userAuthentication
-    /// 券面事項入力補助用
+    /// Electronic Certificate for the Bearer's Signature (署名用電子証明書)
+    case electronicCertificateForTheBearersSignature
+    /// Electronic Certificate for User Identification (利用者証明用電子証明書)
+    case electronicCertificateForUserIdentification
+    /// Card Info Input Support Application (券面事項入力補助用)
     case cardInfoInputSupportApplication
-    /// 個人番号カード用（住基カード用）
-    case individualNumberCard
+    /// Basic Resident Registration (個人番号カード用（住民基本台帳用）)
+    case basicResidentRegistration
 }
 
 extension JapanIndividualNumberCardItem {
     var aid: Data {
         switch self {
-        case .digitalSignature:
+        case .electronicCertificateForTheBearersSignature:
             return Data([0xD3, 0x92, 0xF0, 0x00, 0x26, 0x01, 0x00, 0x00, 0x00, 0x01])
-        case .userAuthentication:
+        case .electronicCertificateForUserIdentification:
             return Data([0xD3, 0x92, 0xF0, 0x00, 0x26, 0x01, 0x00, 0x00, 0x00, 0x01])
         case .cardInfoInputSupportApplication:
             return Data([0xD3, 0x92, 0x10, 0x00, 0x31, 0x00, 0x01, 0x01, 0x04, 0x08])
-        case .individualNumberCard:
+        case .basicResidentRegistration:
             return Data([0xD3, 0x92, 0x10, 0x00, 0x31, 0x00, 0x01, 0x01, 0x01, 0x00])
         }
     }
