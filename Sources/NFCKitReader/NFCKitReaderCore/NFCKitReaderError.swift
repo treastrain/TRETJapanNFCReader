c5ba598b87ae2a42a7b22646984a7960928df844
treastrain / Tanaka Ryoga <tr@tret.jp>
2021-11-09 01:13:01 +0900

    Update NFCKitReaderError.swift

--- Sources/NFCKitReader/NFCKitReaderCore/NFCKitReaderError.swift
+++ Sources/NFCKitReader/NFCKitReaderCore/NFCKitReaderError.swift
@@ -12,6 +12,19 @@ public enum NFCKitReaderError: Error {
     case readingUnavailable
     /// Could not initialize the reader session due to unavailability of system memory resources.
     case systemMemoryResourcesAreUnavailable
-    /// PIN が指定の形式を満たしていないため、スキャンを開始できません
+    /// Cannot start scanning because the PIN does not meet the required format.
     case notStartedScanBecausePINFormatInvalid
+}
+
+extension NFCKitReaderError: LocalizedError {
+    public var errorDescription: String? {
+        switch self {
+        case .readingUnavailable:
+            return "This device does not support scanning for and detecting NFC tags."
+        case .systemMemoryResourcesAreUnavailable:
+            return "Could not initialize the reader session due to unavailability of system memory resources."
+        case .notStartedScanBecausePINFormatInvalid:
+            return "Cannot start scanning because the PIN does not meet the required format."
+        }
+    }
 }
