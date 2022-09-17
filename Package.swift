221604068f34ccd0aa0f4a225d97f97f876decb3
treastrain / Tanaka Ryoga <tr@tret.jp>
2022-09-18 05:05:36 +0900

    Swift Package を作成 (#77)
    * `swift package init`
    
    * Update Package.swift
    
    * Delete TRETNFCKitTests.swift

---
+++ Package.swift
@@ -0,0 +1,52 @@
+// swift-tools-version: 5.7
+
+import PackageDescription
+
+let packageName = "TRETNFCKit"
+
+let swiftSettings: [SwiftSetting] = [
+    .unsafeFlags(
+        [
+            "-Xfrontend", "-warn-concurrency",
+            "-Xfrontend", "-enable-actor-data-race-checks",
+        ], .when(configuration: .debug)
+    ),
+]
+
+var products: [Product] = []
+var targets: [Target] = []
+
+@discardableResult
+func add(moduleName: String, dependencies: [Target.Dependency] = [], includesTest: Bool) -> Target.Dependency {
+    let targetName = "\(packageName)_\(moduleName)"
+    products.append(
+        .library(name: targetName, targets: [targetName])
+    )
+    targets.append(
+        .target(name: targetName, dependencies: dependencies, path: "Sources/\(moduleName)", swiftSettings: swiftSettings)
+    )
+    if includesTest {
+        targets.append(
+            .testTarget(name: "\(packageName)_\(moduleName)Tests", dependencies: [.init(stringLiteral: targetName)], path: "Tests/\(moduleName)Tests", swiftSettings: swiftSettings)
+        )
+    }
+    return .init(stringLiteral: targetName)
+}
+
+// MARK: - Modules
+
+// MARK: - Package
+products.append(
+    .library(name: packageName, targets: [packageName])
+)
+targets.append(
+    .target(name: packageName, dependencies: targets.filter { !$0.isTest }.map { .init(stringLiteral: $0.name) }, swiftSettings: swiftSettings)
+)
+
+let package = Package(
+    name: packageName,
+    defaultLocalization: "en",
+    platforms: [.iOS(.v13), .macOS(.v10_15), .macCatalyst(.v13), .tvOS(.v13), .watchOS(.v6)],
+    products: products,
+    targets: targets
+)
