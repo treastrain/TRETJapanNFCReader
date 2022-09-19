1f4a96c849fc9df5c8f2b806f7f5e05b15d1724f
treastrain / Tanaka Ryoga <tr@tret.jp>
2022-09-20 01:52:29 +0900

    Create TRETNFCKitExample

---
+++ Examples/TRETNFCKitExample/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  TRETNFCKitExample
+//
+//  Created by treastrain on 2022/09/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "wave.3.forward.circle")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Happy tag reading!")
+        }
+        .padding()
+    }
+}
