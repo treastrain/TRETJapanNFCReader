4f0926f90b11727f103f767823bc14e8252db468
treastrain / Tanaka Ryoga <tr@tret.jp>
2022-09-20 02:04:05 +0900

    サンプル用 iOS App ターゲットを追加 (#79)
    * Create TRETNFCKitExample

---
+++ Examples/TRETNFCKitExample/TRETNFCKitExampleApp.swift
@@ -0,0 +1,18 @@
+//
+//  TRETNFCKitExampleApp.swift
+//  TRETNFCKitExample
+//
+//  Created by treastrain on 2022/09/20.
+//
+
+import TRETNFCKit
+import SwiftUI
+
+@main
+struct TRETNFCKitExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
