374dec6b8396dc43b5f6b7cd61629f7c19fee8b7
treastrain / Tanaka Ryoga <tr@tret.jp>
2022-09-20 03:27:30 +0900

    Update ContentView.swift

--- Examples/TRETNFCKitExample/ContentView.swift
+++ Examples/TRETNFCKitExample/ContentView.swift
@@ -6,8 +6,11 @@
 //
 
 import SwiftUI
+import TRETNFCKit
 
 struct ContentView: View {
+    private let reader = NFCTagReader()
+    
     var body: some View {
         VStack {
             Image(systemName: "wave.3.forward.circle")
@@ -16,5 +19,20 @@ struct ContentView: View {
             Text("Happy tag reading!")
         }
         .padding()
+        .task {
+            do {
+                try await reader.read(pollingOption: .iso18092, detectingAlertMessage: "Place the tag on a flat, non-metal surface and rest your iPhone on the tag.") { session, tags in
+                    let tag = tags.first!
+                    try await session.connect(to: tag)
+                    if case .feliCa(let feliCaTag) = tag {
+                        session.alertMessage = "\(feliCaTag.currentIDm as NSData)"
+                    } else {
+                        session.alertMessage = "No FeliCa tag."
+                    }
+                    return .success
+                }
+            } catch {
+            }
+        }
     }
 }
