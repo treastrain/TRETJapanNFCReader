d519c0fcf302905c42324301758492d803f0d55d
treastrain / Tanaka Ryoga <tr@tret.jp>
2022-09-23 23:07:21 +0900

    Update NFCTagReaderExampleView.swift

--- Examples/TRETNFCKitExample/NFCTagReaderExampleView.swift
+++ Examples/TRETNFCKitExample/NFCTagReaderExampleView.swift
@@ -12,29 +12,26 @@ struct NFCTagReaderExampleView: View {
     private let reader = NFCTagReader()
     
     var body: some View {
-        VStack {
-            Image(systemName: "wave.3.forward.circle")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Happy tag reading!")
-        }
-        .padding()
-        .task {
-            do {
-                try await reader.read(pollingOption: .iso18092, detectingAlertMessage: "Place the tag on a flat, non-metal surface and rest your iPhone on the tag.") { session in
-                    print(session.alertMessage)
-                } didDetect: { session, tags in
-                    let tag = tags.first!
-                    try await session.connect(to: tag)
-                    if case .feliCa(let feliCaTag) = tag {
-                        session.alertMessage = "\(feliCaTag.currentIDm as NSData)"
-                    } else {
-                        session.alertMessage = "No FeliCa tag."
+        List {
+            Button {
+                Task {
+                    try await reader.read(pollingOption: .iso18092, detectingAlertMessage: "Place the tag on a flat, non-metal surface and rest your iPhone on the tag.") { session in
+                        print(session.alertMessage)
+                    } didDetect: { session, tags in
+                        let tag = tags.first!
+                        try await session.connect(to: tag)
+                        if case .feliCa(let feliCaTag) = tag {
+                            session.alertMessage = "\(feliCaTag.currentIDm as NSData)"
+                        } else {
+                            session.alertMessage = "No FeliCa tag."
+                        }
+                        return .success
                     }
-                    return .success
                 }
-            } catch {
+            } label: {
+                Text("Read")
             }
         }
+        .navigationTitle("FeliCa")
     }
 }
