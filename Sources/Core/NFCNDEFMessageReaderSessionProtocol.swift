faf1ecbcbb83eba754a54be6318c8c46bd427086
treastrain / Tanaka Ryoga <tr@tret.jp>
2022-09-23 22:32:47 +0900

    Create NFCNDEFMessageReaderSessionProtocol.swift

---
+++ Sources/Core/NFCNDEFMessageReaderSessionProtocol.swift
@@ -0,0 +1,20 @@
+//
+//  NFCNDEFMessageReaderSessionProtocol.swift
+//  Core
+//
+//  Created by treastrain on 2022/09/23.
+//
+
+import Foundation
+#if canImport(CoreNFC)
+import CoreNFC
+#endif
+
+public protocol NFCNDEFMessageReaderSessionProtocol: NFCNDEFMessageReaderSessionAlertMessageable {
+#if canImport(CoreNFC)
+#endif
+}
+
+#if canImport(CoreNFC)
+extension NFCNDEFReaderSession: NFCNDEFMessageReaderSessionProtocol {}
+#endif
