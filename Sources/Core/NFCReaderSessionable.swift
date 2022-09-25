e4e1f6381b5df4cd8b9b0e314ca3e54a65a74bc0
treastrain / Tanaka Ryoga <tr@tret.jp>
2022-09-26 07:57:28 +0900

    Rename `NFCReaderSessionDelegatable` to `NFCReaderSessionable`

---
+++ Sources/Core/NFCReaderSessionable.swift
@@ -0,0 +1,37 @@
+//
+//  NFCReaderSessionable.swift
+//  Core
+//
+//  Created by treastrain on 2022/09/24.
+//
+
+#if canImport(CoreNFC)
+public protocol NFCReaderSessionable: NFCReaderSession {
+    associatedtype Session
+    associatedtype Delegate
+    associatedtype AlertMessageable: Sendable
+}
+#endif
+
+#if canImport(CoreNFC)
+extension NFCNDEFReaderSession: NFCReaderSessionable, @unchecked Sendable {
+    public typealias Session = NFCNDEFReaderSession
+    public typealias Delegate = NFCNDEFReaderSessionDelegate
+}
+
+extension NFCTagReaderSession: NFCReaderSessionable, @unchecked Sendable {
+    public typealias Session = NFCTagReaderSession
+    public typealias Delegate = NFCTagReaderSessionDelegate
+}
+
+extension NFCVASReaderSession: NFCReaderSessionable, @unchecked Sendable {
+    public typealias Session = NFCVASReaderSession
+    public typealias Delegate = NFCVASReaderSessionDelegate
+}
+#endif
+
+#if canImport(CoreNFC)
+extension NFCReaderSessionable {
+    public typealias AlertMessageable = any NFCReaderSessionAlertMessageable
+}
+#endif
