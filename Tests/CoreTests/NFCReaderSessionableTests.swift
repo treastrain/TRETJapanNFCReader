aa93484c4a76d81aa217c0eb52eea23bbbc86d62
treastrain / Tanaka Ryoga <tr@tret.jp>
2022-10-12 05:06:15 +0900

    `NFCReaderSessionableTests` を更新 (#88)
    * Update NFCReaderSessionableTests.swift

--- Tests/CoreTests/NFCReaderSessionableTests.swift
+++ Tests/CoreTests/NFCReaderSessionableTests.swift
@@ -13,7 +13,7 @@ import CoreNFC
 @testable import TRETNFCKit_Core
 
 final class NFCReaderSessionableTests: XCTestCase {
-    func testNFCReaderSessionableInheritedByNFCNDEFReaderSession() throws {
+    func testNFCNDEFReaderSessionConformsToNFCReaderSessionable() throws {
         #if canImport(CoreNFC)
         XCTAssertIdentical(NFCNDEFReaderSession.Session.self, NFCNDEFReaderSession.self)
         XCTAssertIdentical(NFCNDEFReaderSession.Delegate.self as AnyObject, NFCNDEFReaderSessionDelegate.self as AnyObject)
@@ -22,16 +22,16 @@ final class NFCReaderSessionableTests: XCTestCase {
         #endif
     }
     
-    func testNFCReaderSessionableInheritedByNFCTagReaderSession() throws {
+    func testNFCTagReaderSessionConformsToNFCReaderSessionable() throws {
         #if canImport(CoreNFC)
         XCTAssertIdentical(NFCTagReaderSession.Session.self, NFCTagReaderSession.self)
-        // XCTAssertIdentical(NFCTagReaderSession.Delegate.self as AnyObject, NFCTagReaderSessionDelegate.self as AnyObject) // TODO: wait fixing https://gist.github.com/treastrain/6e9358d3e715720568004d99aabb25fc
+        XCTAssertEqual(String(describing: NFCTagReaderSession.Delegate.self), String(describing: NFCTagReaderSessionDelegate.self)) // XCTAssertIdentical(NFCTagReaderSession.Delegate.self as AnyObject, NFCTagReaderSessionDelegate.self as AnyObject) // TODO: wait fixing https://gist.github.com/treastrain/6e9358d3e715720568004d99aabb25fc
         #else
         throw XCTSkip("Support for this platform is not considered.")
         #endif
     }
     
-    func testNFCReaderSessionableInheritedByNFCVASReaderSession() throws {
+    func testNFCVASReaderSessionConformsToNFCReaderSessionable() throws {
         #if canImport(CoreNFC)
         XCTAssertIdentical(NFCVASReaderSession.Session.self, NFCVASReaderSession.self)
         XCTAssertIdentical(NFCVASReaderSession.Delegate.self as AnyObject, NFCVASReaderSessionDelegate.self as AnyObject)
