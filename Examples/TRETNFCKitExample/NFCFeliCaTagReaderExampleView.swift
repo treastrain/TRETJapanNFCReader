284fd3939ebc835438f993fca86b3819f86fa876
treastrain / Tanaka Ryoga <tr@tret.jp>
2023-01-26 03:12:29 +0900

    `FeliCaTagReader` 向けの `SwiftUI.ViewModifier` を追加 (#107)
    * Create FeliCaTagReaderViewModifier.swift
    
    * `FeliCaTagReaderViewModifier` の使用例を追加

--- Examples/TRETNFCKitExample/NFCFeliCaTagReaderExampleView.swift
+++ Examples/TRETNFCKitExample/NFCFeliCaTagReaderExampleView.swift
@@ -9,16 +9,42 @@ import SwiftUI
 import TRETNFCKit_FeliCa
 
 struct NFCFeliCaTagReaderExampleView: View {
+    @State private var isPresented = false
     @ObservedObject var viewModel = ViewModel()
     
     var body: some View {
         List {
+            Button {
+                isPresented = true
+            } label: {
+                Text("Read (using view modifier)")
+            }
             Button {
                 viewModel.read()
             } label: {
                 Text("Read")
             }
         }
+        .feliCaTagReader(
+            isPresented: $isPresented,
+            detectingAlertMessage: "Place the tag on a flat, non-metal surface and rest your iPhone on the tag.",
+            onBeginReadingError: { error in
+                print(error)
+            },
+            didBecomeActive: { session in
+                print(session.alertMessage)
+            },
+            didInvalidate: { error in
+                print(error)
+            },
+            didDetect: { session, tags in
+                let tag = tags.first!
+                let feliCaTag = try await session.connectAsFeliCaTag(to: tag)
+                let (idm, systemCode) = try await feliCaTag.polling(systemCode: Data([0xFE, 0x00]), requestCode: .systemCode, timeSlot: .max1)
+                session.alertMessage = "\(systemCode as NSData)\n\(idm as NSData)"
+                return .success
+            }
+        )
         .navigationTitle("FeliCa")
     }
 }
