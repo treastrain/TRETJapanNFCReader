dc2c62281d4a245f45e1fe52affd4185cb34d7bb
treastrain / Tanaka Ryoga <tr@tret.jp>
2023-01-26 08:01:09 +0900

    `MiFareTagReader` 向けの `SwiftUI.ViewModifier` を追加 (#110)
    * Create MiFareTagReaderViewModifier.swift
    
    * `MiFareTagReaderViewModifier` の使用例を追加

--- Examples/TRETNFCKitExample/NFCMiFareTagReaderExampleView.swift
+++ Examples/TRETNFCKitExample/NFCMiFareTagReaderExampleView.swift
@@ -9,16 +9,41 @@ import SwiftUI
 import TRETNFCKit_MiFare
 
 struct NFCMiFareTagReaderExampleView: View {
+    @State private var isPresented = false
     @ObservedObject var viewModel = ViewModel()
     
     var body: some View {
         List {
+            Button {
+                isPresented = true
+            } label: {
+                Text("Read (using view modifier)")
+            }
             Button {
                 viewModel.read()
             } label: {
                 Text("Read")
             }
         }
+        .miFareTagReader(
+            isPresented: $isPresented,
+            detectingAlertMessage: "Place the tag on a flat, non-metal surface and rest your iPhone on the tag.",
+            onBeginReadingError: { error in
+                print(error)
+            },
+            didBecomeActive: { session in
+                print(session.alertMessage)
+            },
+            didInvalidate: { error in
+                print(error)
+            },
+            didDetect: { session, tags in
+                let tag = tags.first!
+                let miFareTag = try await session.connectAsMiFareTag(to: tag)
+                session.alertMessage = "\(miFareTag.identifier as NSData)"
+                return .success
+            }
+        )
         .navigationTitle("MiFare")
     }
 }
