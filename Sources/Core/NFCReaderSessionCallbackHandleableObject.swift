abde015979e7aa7aa24e3cb96ce4c503c92d315a
treastrain / Tanaka Ryoga <tr@tret.jp>
2023-02-05 22:06:20 +0900

    `NFCReaderSessionCallbackHandleableObject.taskPriority` を追加

--- Sources/Core/NFCReaderSessionCallbackHandleableObject.swift
+++ Sources/Core/NFCReaderSessionCallbackHandleableObject.swift
@@ -8,6 +8,7 @@
 public protocol NFCReaderSessionCallbackHandleableObject: Actor {
     #if canImport(CoreNFC)
     associatedtype TagType: NFCTagType
+    nonisolated var taskPriority: TaskPriority? { get }
     var didBecomeActiveHandler: (_ session: TagType.ReaderSession.AfterBeginProtocol) -> Void { get }
     var didInvalidateHandler: (_ error: NFCReaderError) -> Void { get }
     var didDetectHandler: (_ session: TagType.ReaderSessionProtocol, _ object: TagType.ReaderSessionDetectObject) async throws -> TagType.DetectResult { get }
@@ -17,13 +18,13 @@ public protocol NFCReaderSessionCallbackHandleableObject: Actor {
 #if canImport(CoreNFC)
 extension NFCReaderSessionCallbackHandleableObject {
     public nonisolated func didBecomeActive(_ session: TagType.ReaderSession.AfterBeginProtocol) {
-        Task {
+        Task(priority: taskPriority) {
             await didBecomeActive(session: session)
         }
     }
     
     public nonisolated func didInvalidateWithError(_ session: TagType.ReaderSessionProtocol, error: Error) {
-        Task {
+        Task(priority: taskPriority) {
             await didInvalidateWithError(session: session, error: error)
         }
     }
