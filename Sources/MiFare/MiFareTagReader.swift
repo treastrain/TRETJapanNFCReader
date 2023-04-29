2be76431a9cc506a4ea304c8057018831a25b1f0
treastrain / Tanaka Ryoga <tr@tret.jp>
2023-04-30 00:58:04 +0900

    Update MiFareTagReader for actor NativeTag reader

--- Sources/MiFare/MiFareTagReader.swift
+++ Sources/MiFare/MiFareTagReader.swift
@@ -9,14 +9,14 @@ public typealias MiFareTagReader = NFCReader<NativeTag>
 
 extension MiFareTagReader {
     #if canImport(CoreNFC)
-    public typealias ReaderSessionProtocol = _MiFareTagReaderOpaqueTypeBuilder.ReaderSessionProtocol // it means like `some MiFareTagReaderSessionProtocol`
+    public typealias ReaderProtocol = _OpaqueTypeBuilder.ReaderProtocol // it means like `some MiFareTagReaderProtocol`
     #endif
 }
 
 #if canImport(CoreNFC)
-public enum _MiFareTagReaderOpaqueTypeBuilder: _NFCTagTypeOpaqueTypeBuilderProtocol {
-    public var readerSessionProtocol: some MiFareTagReaderSessionProtocol {
-        NativeTag.ReaderSession(pollingOption: [], delegate: _NFCTagReaderSessionOpaqueTypeBuilder())!
+public enum _OpaqueTypeBuilder: _NFCTagTypeOpaqueTypeBuilderProtocol {
+    public var readerProtocol: some MiFareTagReaderProtocol {
+        NativeTag.Reader(pollingOption: [], delegate: { fatalError("Do not call this property.") }(), taskPriority: nil)!
     }
 }
 #endif
@@ -26,9 +26,9 @@ extension MiFareTagReader {
     public func read(
         taskPriority: TaskPriority? = nil,
         detectingAlertMessage: String,
-        didBecomeActive: @escaping @Sendable (_ session: TagType.ReaderSession.AfterBeginProtocol) -> Void = { _ in },
-        didInvalidate: @escaping @Sendable (NFCReaderError) -> Void = { _ in },
-        didDetect: @escaping @Sendable (_ session: ReaderSessionProtocol, _ tags: TagType.ReaderSessionDetectObject) async throws -> TagType.DetectResult
+        didBecomeActive: @escaping @Sendable (_ reader: TagType.Reader.AfterBeginProtocol) async -> Void = { _ in },
+        didInvalidate: @escaping @Sendable (_ error: NFCReaderError) -> Void = { _ in },
+        didDetect: @escaping @Sendable (_ reader: ReaderProtocol, _ tags: TagType.ReaderDetectObject) async throws -> TagType.DetectResult
     ) async throws {
         try await read(
             pollingOption: .iso14443,
@@ -36,7 +36,7 @@ extension MiFareTagReader {
             detectingAlertMessage: detectingAlertMessage,
             didBecomeActive: didBecomeActive,
             didInvalidate: didInvalidate,
-            didDetect: { try await didDetect($0 as! ReaderSessionProtocol, $1) }
+            didDetect: { try await didDetect($0 as! ReaderProtocol, $1) }
         )
     }
     #endif
