a5bfe8e65e5042da3d960f08c5bfb79af07bbfab
treastrain / Tanaka Ryoga <tr@tret.jp>
2024-01-29 01:25:05 +0900

    Use `AsyncNFCTagReaderSession` with `NFCMiFareTagReaderExampleView`

--- Examples/TRETNFCKitExample/NFCMiFareTagReaderExampleView.swift
+++ Examples/TRETNFCKitExample/NFCMiFareTagReaderExampleView.swift
@@ -6,11 +6,13 @@
 //
 
 import SwiftUI
+import TRETNFCKit_Async
 import TRETNFCKit_MiFare
 
 struct NFCMiFareTagReaderExampleView: View {
     @State private var isPresented = false
     @ObservedObject var viewModel = ViewModel()
+    @State private var readerSession: AsyncNFCTagReaderSession?
     
     var body: some View {
         List {
@@ -24,8 +26,14 @@ struct NFCMiFareTagReaderExampleView: View {
                     try await viewModel.read()
                 }
             } label: {
-                Text("Read")
+                Text("Read (using reader)")
             }
+            Button {
+                readerSession = AsyncNFCTagReaderSession(pollingOption: .iso14443)
+            } label: {
+                Text("Read (using async stream)")
+            }
+            .disabled(readerSession != nil)
         }
         .miFareTagReader(
             isPresented: $isPresented,
@@ -46,6 +54,39 @@ struct NFCMiFareTagReaderExampleView: View {
                 return .success
             }
         )
+        .task(id: readerSession == nil) {
+            defer { readerSession = nil }
+            guard let readerSession else { return }
+            guard AsyncNFCTagReaderSession.readingAvailable else { return }
+            
+            for await event in readerSession.eventStream {
+                switch event {
+                case .sessionIsReady:
+                    readerSession.alertMessage = "Place the tag on a flat, non-metal surface and rest your iPhone on the tag."
+                    readerSession.start()
+                case .sessionStarted:
+                    break
+                case .sessionBecomeActive:
+                    break
+                case .sessionDetected(let tags):
+                    do {
+                        let tag = tags.first!
+                        guard case .miFare(let miFareTag) = tag else {
+                            throw NFCReaderError(.readerErrorInvalidParameter)
+                        }
+                        try await readerSession.connect(to: tag)
+                        readerSession.alertMessage = "\(miFareTag.identifier as NSData)"
+                        readerSession.stop()
+                    } catch {
+                        readerSession.stop(errorMessage: error.localizedDescription)
+                    }
+                case .sessionCreationFailed(let reason):
+                    print(reason)
+                case .sessionInvalidated(let reason):
+                    print(reason)
+                }
+            }
+        }
         .navigationTitle("MiFare")
     }
 }
