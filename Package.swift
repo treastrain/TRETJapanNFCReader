ae79f418ee537dc53fae4728308ca03459aa1c60
treastrain / Tanaka Ryoga <tr@tret.jp>
2024-09-15 00:34:40 +0900

    Use swift-tools-version 6.0

--- Package.swift
+++ Package.swift
@@ -1,39 +1,30 @@
-// swift-tools-version: 5.9
+// swift-tools-version: 6.0
 
 import PackageDescription
 
 let packageName = "TRETNFCKit"
 
 extension SwiftSetting {
-    static let forwardTrailingClosures: Self = .enableUpcomingFeature("ForwardTrailingClosures")
-    static let strictConcurrency: Self = .enableUpcomingFeature("StrictConcurrency")
-    static let existentialAny: Self = .enableUpcomingFeature("ExistentialAny")
-    static let bareSlashRegexLiterals: Self = .enableUpcomingFeature("BareSlashRegexLiterals")
-    static let conciseMagicFile: Self = .enableUpcomingFeature("ConciseMagicFile")
-    static let importObjcForwardDeclarations: Self = .enableUpcomingFeature("ImportObjcForwardDeclarations")
-    static let disableOutwardActorInference: Self = .enableUpcomingFeature("DisableOutwardActorInference")
+    static let existentialAny: Self = .enableUpcomingFeature("ExistentialAny")                      // SE-0335, Swift 5.6,  SwiftPM 5.8+
+    static let internalImportsByDefault: Self = .enableUpcomingFeature("InternalImportsByDefault")  // SE-0409, Swift 6.0,  SwiftPM 6.0+
 }
 
 let swiftSettings: [SwiftSetting] = [
-    .forwardTrailingClosures,
-    .strictConcurrency,
     .existentialAny,
-    .bareSlashRegexLiterals,
-    .conciseMagicFile,
-    .importObjcForwardDeclarations,
-    .disableOutwardActorInference,
+    .internalImportsByDefault,
 ]
 
 var products: [Product] = []
 var targets: [Target] = []
 
 @discardableResult
-func add(moduleName: String, dependencies: [Target.Dependency] = [], includesTest: Bool) -> Target.Dependency {
+@MainActor func add(moduleName: String, dependencies: [Target.Dependency] = [], includesTest: Bool, swiftLanguageMode: SwiftLanguageMode) -> Target.Dependency {
     let targetName = "\(packageName)_\(moduleName)"
     let target = Target.Dependency(stringLiteral: targetName)
     products.append(
         .library(name: targetName, targets: [targetName])
     )
+    let swiftSettings = swiftSettings + [.swiftLanguageMode(swiftLanguageMode)]
     targets.append(
         .target(name: targetName, dependencies: dependencies, path: "Sources/\(moduleName)", swiftSettings: swiftSettings)
     )
@@ -46,21 +37,21 @@ func add(moduleName: String, dependencies: [Target.Dependency] = [], includesTes
 }
 
 // MARK: - Modules - Tools for DEBUG
-let assertServices = add(moduleName: "AssertServices", includesTest: false)
-let infoPListChecker = add(moduleName: "InfoPListChecker", includesTest: false)
+let assertServices = add(moduleName: "AssertServices", includesTest: false, swiftLanguageMode: .v5)
+let infoPListChecker = add(moduleName: "InfoPListChecker", includesTest: false, swiftLanguageMode: .v5)
 
 // MARK: - Modules - Primary
-let async = add(moduleName: "Async", includesTest: false)
-let core = add(moduleName: "Core", dependencies: [assertServices, infoPListChecker], includesTest: true)
-let nativeTag = add(moduleName: "NativeTag", dependencies: [core], includesTest: true)
-add(moduleName: "NDEFMessage", dependencies: [core], includesTest: true)
-add(moduleName: "NDEFTag", dependencies: [core], includesTest: true)
+let async = add(moduleName: "Async", includesTest: false, swiftLanguageMode: .v5)
+let core = add(moduleName: "Core", dependencies: [assertServices, infoPListChecker], includesTest: true, swiftLanguageMode: .v5)
+let nativeTag = add(moduleName: "NativeTag", dependencies: [core], includesTest: true, swiftLanguageMode: .v5)
+add(moduleName: "NDEFMessage", dependencies: [core], includesTest: true, swiftLanguageMode: .v5)
+add(moduleName: "NDEFTag", dependencies: [core], includesTest: true, swiftLanguageMode: .v5)
 
 // MARK: - Modules - Secondary
-add(moduleName: "FeliCa", dependencies: [nativeTag], includesTest: true)
-add(moduleName: "ISO7816", dependencies: [nativeTag], includesTest: true)
-add(moduleName: "ISO15693", dependencies: [nativeTag], includesTest: true)
-add(moduleName: "MiFare", dependencies: [nativeTag], includesTest: true)
+add(moduleName: "FeliCa", dependencies: [nativeTag], includesTest: true, swiftLanguageMode: .v5)
+add(moduleName: "ISO7816", dependencies: [nativeTag], includesTest: true, swiftLanguageMode: .v5)
+add(moduleName: "ISO15693", dependencies: [nativeTag], includesTest: true, swiftLanguageMode: .v5)
+add(moduleName: "MiFare", dependencies: [nativeTag], includesTest: true, swiftLanguageMode: .v5)
 
 // MARK: - Package
 products.append(
