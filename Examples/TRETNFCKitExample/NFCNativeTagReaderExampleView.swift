711238cab9f2a36ef6d1eaa79d56d49802c507db
treastrain / Tanaka Ryoga <tr@tret.jp>
2024-10-20 02:23:41 +0900

    Replace `NFCNativeTagReaderViewModifier ` with async sequence

--- Examples/TRETNFCKitExample/NFCNativeTagReaderExampleView.swift
+++ Examples/TRETNFCKitExample/NFCNativeTagReaderExampleView.swift
@@ -21,6 +21,7 @@ struct NFCNativeTagReaderExampleView: View {
             } label: {
                 Text("Read (using view modifier)")
             }
+            .disabled(!NFCTagReaderSession.readingAvailable || isPresented)
             Button {
                 Task {
                     try await viewModel.read()
@@ -33,37 +34,40 @@ struct NFCNativeTagReaderExampleView: View {
             } label: {
                 Text("Read (using async stream)")
             }
-            .disabled(readerSession != nil)
+            .disabled(!NFCTagReaderSession.readingAvailable || readerSession != nil)
         }
         .nfcNativeTagReader(
             isPresented: $isPresented,
             pollingOption: [.iso14443, .iso15693, .iso18092],
             detectingAlertMessage: "Place the tag on a flat, non-metal surface and rest your iPhone on the tag.",
-            onBeginReadingError: { error in
-                print(error)
+            terminatingAlertMessage: "CANCELLED",
+            onDidBecomeActive: { readerSession in
+                print(readerSession.alertMessage)
             },
-            didBecomeActive: { reader in
-                await print(reader.alertMessage)
-            },
-            didInvalidate: { error in
-                print(error)
-            },
-            didDetect: { reader, tags in
-                let tag = tags.first!
-                try await reader.connect(to: tag)
-                switch tag {
-                case .feliCa(let feliCaTag):
-                    await reader.set(alertMessage: "FeliCa\n\(feliCaTag.currentIDm as NSData)")
-                case .iso7816(let iso7816Tag):
-                    await reader.set(alertMessage: "ISO14443-4 type A / B tag with ISO7816\n\(iso7816Tag.identifier as NSData)")
-                case .iso15693(let iso15693Tag):
-                    await reader.set(alertMessage: "ISO 15693\n\(iso15693Tag.identifier as NSData)")
-                case .miFare(let miFareTag):
-                    await reader.set(alertMessage: "MiFare technology tag (MIFARE Plus, UltraLight, DESFire) base on ISO14443\n\(miFareTag.identifier as NSData)")
-                @unknown default:
-                    await reader.set(alertMessage: "Unknown tag.")
+            onDidDetect: { readerSession, tags in
+                print(tags)
+                do {
+                    let tag = tags.first!
+                    try await readerSession.connect(to: tag)
+                    switch tag {
+                    case .feliCa(let feliCaTag):
+                        readerSession.alertMessage = "FeliCa\n\(feliCaTag.currentIDm as NSData)"
+                    case .iso7816(let iso7816Tag):
+                        readerSession.alertMessage = "ISO14443-4 type A / B tag with ISO7816\n\(iso7816Tag.identifier as NSData)"
+                    case .iso15693(let iso15693Tag):
+                        readerSession.alertMessage = "ISO 15693\n\(iso15693Tag.identifier as NSData)"
+                    case .miFare(let miFareTag):
+                        readerSession.alertMessage = "MiFare technology tag (MIFARE Plus, UltraLight, DESFire) base on ISO14443\n\(miFareTag.identifier as NSData)"
+                    @unknown default:
+                        readerSession.alertMessage = "Unknown tag."
+                    }
+                    readerSession.invalidate()
+                } catch {
+                    readerSession.invalidate(errorMessage: error.localizedDescription)
                 }
-                return .success
+            },
+            onDidInvalidate: { error in
+                print(error)
             }
         )
         .task(id: readerSession == nil) {
