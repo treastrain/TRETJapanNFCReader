f91b7682ee45e81b02a4eea136c1a036a71d277a
treastrain / Tanaka Ryoga <tr@tret.jp>
2024-10-20 02:36:08 +0900

    Replace `FeliCaTagReaderViewModifier` with async sequence

--- Examples/TRETNFCKitExample/NFCFeliCaTagReaderExampleView.swift
+++ Examples/TRETNFCKitExample/NFCFeliCaTagReaderExampleView.swift
@@ -11,7 +11,6 @@ import TRETNFCKit_FeliCa
 
 struct NFCFeliCaTagReaderExampleView: View {
     @State private var isPresented = false
-    @ObservedObject var viewModel = ViewModel()
     @State private var readerSession: NFCTagReaderSession?
     
     var body: some View {
@@ -21,38 +20,34 @@ struct NFCFeliCaTagReaderExampleView: View {
             } label: {
                 Text("Read (using view modifier)")
             }
-            Button {
-                Task {
-                    try await viewModel.read()
-                }
-            } label: {
-                Text("Read (using reader)")
-            }
+            .disabled(!NFCTagReaderSession.readingAvailable || isPresented)
             Button {
                 readerSession = NFCTagReaderSession(pollingOption: .iso18092)
             } label: {
                 Text("Read (using async stream)")
             }
-            .disabled(readerSession != nil)
+            .disabled(!NFCTagReaderSession.readingAvailable || readerSession != nil)
         }
         .feliCaTagReader(
             isPresented: $isPresented,
             detectingAlertMessage: "Place the tag on a flat, non-metal surface and rest your iPhone on the tag.",
-            onBeginReadingError: { error in
+            terminatingAlertMessage: "CANCELLED",
+            onDidBecomeActive: { readerSession in
+                print(readerSession.alertMessage)
+            },
+            onDidDetect: { readerSession, tags in
+                do {
+                    let tag = tags.first!
+                    let feliCaTag = try await readerSession.connectAsFeliCaTag(to: tag)
+                    let (idm, systemCode) = try await feliCaTag.polling(systemCode: Data([0xFE, 0x00]), requestCode: .systemCode, timeSlot: .max1)
+                    readerSession.alertMessage = "\(systemCode as NSData)\n\(idm as NSData)"
+                    readerSession.invalidate()
+                } catch {
+                    readerSession.invalidate(errorMessage: error.localizedDescription)
+                }
+            },
+            onDidInvalidate: { error in
                 print(error)
-            },
-            didBecomeActive: { reader in
-                await print(reader.alertMessage)
-            },
-            didInvalidate: { error in
-                print(error)
-            },
-            didDetect: { reader, tags in
-                let tag = tags.first!
-                let feliCaTag = try await reader.connectAsFeliCaTag(to: tag)
-                let (idm, systemCode) = try await feliCaTag.polling(systemCode: Data([0xFE, 0x00]), requestCode: .systemCode, timeSlot: .max1)
-                await reader.set(alertMessage: "\(systemCode as NSData)\n\(idm as NSData)")
-                return .success
             }
         )
         .task(id: readerSession == nil) {
@@ -67,10 +62,7 @@ struct NFCFeliCaTagReaderExampleView: View {
                 case .sessionDetected(let tags):
                     do {
                         let tag = tags.first!
-                        guard case .feliCa(let feliCaTag) = tag else {
-                            throw NFCReaderError(.readerErrorInvalidParameter)
-                        }
-                        try await readerSession.connect(to: tag)
+                        let feliCaTag = try await readerSession.connectAsFeliCaTag(to: tag)
                         let (idm, systemCode) = try await feliCaTag.polling(systemCode: Data([0xFE, 0x00]), requestCode: .systemCode, timeSlot: .max1)
                         readerSession.alertMessage = "\(systemCode as NSData)\n\(idm as NSData)"
                         readerSession.invalidate()
@@ -83,31 +75,5 @@ struct NFCFeliCaTagReaderExampleView: View {
             }
         }
         .navigationTitle("FeliCa")
-    }
-}
-
-extension NFCFeliCaTagReaderExampleView {
-    final class ViewModel : ObservableObject {
-        private var reader: FeliCaTagReader!
-        
-        func read() async throws {
-            reader = .init()
-            try await reader.read(
-                detectingAlertMessage: "Place the tag on a flat, non-metal surface and rest your iPhone on the tag.",
-                didBecomeActive: { reader in
-                    await print(reader.alertMessage)
-                },
-                didInvalidate: { error in
-                    print(error)
-                },
-                didDetect: { reader, tags in
-                    let tag = tags.first!
-                    let feliCaTag = try await reader.connectAsFeliCaTag(to: tag)
-                    let (idm, systemCode) = try await feliCaTag.polling(systemCode: Data([0xFE, 0x00]), requestCode: .systemCode, timeSlot: .max1)
-                    await reader.set(alertMessage: "\(systemCode as NSData)\n\(idm as NSData)")
-                    return .success
-                }
-            )
-        }
     }
 }
